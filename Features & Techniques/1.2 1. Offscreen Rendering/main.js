1d45d1c9339e8eead5c27b742b03b390ac3c854b
Himanshu Mishra <mhimanshu712@gmail.com>
2020-02-29 12:49:29 +0530

    New Folder Added

---
+++ Features & Techniques/1.2 1. Offscreen Rendering/main.js
@@ -0,0 +1,64 @@
+// Modules
+// This will be rendered offscreen, so no content will be visible
+// This is mainly used to get the screenshot of rendered content.
+// Offscreen rendering is faster
+const {app, BrowserWindow} = require('electron')
+const fs = require('fs')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+app.disableHardwareAcceleration()
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    //show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      offscreen: true
+    }
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadURL('https://electronjs.org')
+
+  let i = 1
+  mainWindow.webContents.on('paint', (e, dirty, image) => {
+
+    let screenshot = image.toPNG()
+    fs.writeFile( app.getPath('desktop') + `/screenshot_${i}.png`, screenshot, console.log )
+    i++
+  })
+
+  mainWindow.webContents.on('did-finish-load', e => {
+    console.log( mainWindow.getTitle() )
+
+    mainWindow.close()
+    mainWindow = null
+  })
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  // mainWindow.on('closed',  () => {
+  //   mainWindow = null
+  // })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
